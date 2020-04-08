686242a04b35b34590c47afce4dcd71f028354ad
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2020-04-08 11:40:04 +0200

    Add files via upload

---
+++ kusanovicLenardNebeskoTijelo/kusanovicLenardNebeskoTijelo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardNebeskoTijelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet Zemlja = new Planet(2, 2);
+            Satelit Mjesec = new Satelit(4, 2);
+
+            Console.WriteLine("\nBrzina oko osi: " + Zemlja.BrzinaOkoOsi + "\nBrzina oko Sunca: " + Zemlja.BrzinaOkoSunca);
+            Console.WriteLine("\nBrzina oko osi: " + Mjesec.BrzinaOkoOsi + "\nBrzina oko Sunca: " + Mjesec.BrzinaOkoSunca);
+
+            Console.ReadKey();
+        }
+    }
+}
